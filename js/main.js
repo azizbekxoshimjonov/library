eec5ea0996e02aab60cfd57d3845c10c07b46ee0
azizbekxoshimjonov <azizbekxoshimjonov.veb@gmail.com>
2024-03-09 10:27:14 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+//https://65eaf57143ce16418932e936.mockapi.io/library
+
+
+function setBoock(e){
+    e.preventDefault()
+    let boock = {
+        title: e.target.title.value,
+        img: e.target.img.value,
+        price: e.target.price.value,
+
+    }
+    fetch('https://65eaf57143ce16418932e936.mockapi.io/library',{
+        method:'POST',
+        headers:{'content-type':'application/json'},
+        body:JSON.stringify(boock)
+    })
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
+}
+let elList = document.querySelector('.list')
+fetch('https://65eaf57143ce16418932e936.mockapi.io/library')
+    .then((res)=> res.json())
+    .then((data)=> {
+        data.forEach(element => {
+            let newLi = document.createElement('li')
+            newLi.innerHTML = `
+            <img src=${element.img} width="100px" alt="">
+            <p>${element.price}</p>
+            <button onclick="DelCar(${element.id})">${element.title}</button>`
+            elList.appendChild(newLi)
+
+            
+        });
+
+    })
+
+    function DelCar(id){
+        fetch(`https://65eaf57143ce16418932e936.mockapi.io/library/${id}`,{
+            method:'DELETE',
+            headers:{'content-type':'application/json'},
+          
+        })
+        .then((res)=> res.json())
+        .then((data) => console.log(data))
+
+    }
